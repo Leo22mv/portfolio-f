943d9410bea7f7c379acbde34956a79b0153f007
Kbza <g.leonel.m17@gmail.com>
2023-01-18 14:26:35 -0300

    Switch para tema

--- src/app/componentes/nav/nav.component.ts
+++ src/app/componentes/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  toggle = document.querySelector("form-check form-witch")
+
+  navLight: string = "navbar bg-light fixed-top"
+  offCanvasTema: string = "offcanvas offcanvas-top"
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  cambiarTema() {
+    this.toggle?.classList.toggle("checked");
+    if (this.toggle?.classList.contains("checked")) {
+      this.navLight = "navbar bg-dark fixed-top"
+      this.offCanvasTema = "offcanvas offcanvas-top text-bg-dark"
+    }
+  }
 }
