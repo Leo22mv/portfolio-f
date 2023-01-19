5858420b7f5bf99555c0070b32a7eb75ce814075
Kbza <g.leonel.m17@gmail.com>
2023-01-19 01:37:37 -0300

    0.1.5 - Tema Dark funcional en nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './componentes/paginas/error404/error404.component';
 import { InicioComponent } from './componentes/paginas/inicio/inicio.component';
+import { ProyectosComponent } from './componentes/paginas/proyectos/proyectos.component';
 
 const routes: Routes = [
+  {path: "proyectos", component: ProyectosComponent},
   {path: "inicio", component: InicioComponent},
-  {path: "", redirectTo: "/inicio", pathMatch: "full"}
+  {path: "", redirectTo: "/inicio", pathMatch: "full"},
+  {path: "**", component: Error404Component}
 ];
 
 @NgModule({
--- src/app/componentes/nav/nav.component.ts
+++ src/app/componentes/nav/nav.component.ts
@@ -7,10 +7,11 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  toggle = document.querySelector("form-check form-witch")
+  temaDark: boolean = false;
 
-  navLight: string = "navbar bg-light fixed-top"
+  navTema: string = "navbar bg-light fixed-top"
   offCanvasTema: string = "offcanvas offcanvas-top"
+  colorTexto: string = "textoLight";
 
   constructor() { }
 
@@ -18,10 +19,15 @@ export class NavComponent implements OnInit {
   }
 
   cambiarTema() {
-    this.toggle?.classList.toggle("checked");
-    if (this.toggle?.classList.contains("checked")) {
-      this.navLight = "navbar bg-dark fixed-top"
-      this.offCanvasTema = "offcanvas offcanvas-top text-bg-dark"
+    if (this.temaDark) {
+      this.navTema = "navbar bg-light fixed-top";
+      this.offCanvasTema = "offcanvas offcanvas-top";  
+      this.colorTexto = "textoLight";
+    } else {
+      this.navTema = "navbar bg-dark fixed-top";
+      this.offCanvasTema = "offcanvas offcanvas-top show text-bg-dark";
+      this.colorTexto = "textoDark";
     }
+    this.temaDark = !this.temaDark
   }
 }
