7ebdb4036e0e3bbc8a78e10fd5da18427a21f2aa
Kbza <g.leonel.m17@gmail.com>
2024-05-28 22:20:37 -0300

    2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
 
   {path: "home", component: HomeComponent},
 
-  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "", redirectTo: "/old_version/inicio", pathMatch: "full"},
   {path: "**", redirectTo: "/old_version/**", pathMatch: "full"}
 ];
 
